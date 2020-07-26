ae175d4b97055be6421d587596f8538d18dfd948
jermaine007 <i77@idea7.top>
2020-07-26 16:12:21 +0800

    [Add] attach develop tools

--- BomTool.NetCore/BomTool.NetCore/Framework/LightWindowBase.cs
+++ BomTool.NetCore/BomTool.NetCore/Framework/LightWindowBase.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
@@ -5,6 +7,10 @@ namespace BomTool.NetCore.Framework
 {
     public class LightWindowBase : Window, IContainerProvider, ILoggerProvider
     {
-        
+        protected LightWindowBase() => EnableDevelopTools();
+
+        [Conditional("DEBUG")]
+        protected void EnableDevelopTools() => this.AttachDevTools();
+
     }
 }
